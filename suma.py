cfbbe5fd4ac933967382a21ac3e89dd5f37e2bf4
MarioBarrantes <MarioBarrantes>
2022-05-24 16:33:03 -0500

    UpDate

--- suma.py
+++ suma.py
@@ -1,6 +1,6 @@
 n1 = input (float ("Ingrese el primero numero"))
 n2 = input (float ("Ingrese el segundo numero"))
 
-op = n1 + n2
+op = n2 + n1
 
 print (f"el resultado de los numeros agregados es = {op}")
