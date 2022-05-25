311b6a79076bbb28548818d21abd7b361d6d758a
MarioBarrantes <barrantesm632@gmail.com>
2022-05-24 20:12:15 -0500

    commit six

--- Exponenciacion.py
+++ Exponenciacion.py
@@ -1,6 +1,4 @@
 nbase = input (float ("Ingrese el primero numero"))
 nexpo = input (float ("Ingrese el segundo numero"))
-
 op = nbase ** nexpo
-
 print (f"el resultado de la Exponenciacion es = {op}")
