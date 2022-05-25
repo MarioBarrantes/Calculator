ecb6d4185927196edb45d8a9bb88c8ba7f0c64ae
MarioBarrantes <barrantesm632@gmail.com>
2022-05-24 19:28:34 -0500

    commit init

--- Suma.py
+++ Suma.py
@@ -1,1 +1,6 @@
-print ("Hello World")
+n1 = input (float ("Ingrese el primero numero"))
+n2 = input (float ("Ingrese el segundo numero"))
+
+op = n2 + n1
+
+print (f"el resultado de los numeros agregados es = {op}")
